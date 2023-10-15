3db76e6e201f57f19924ad3d66c30549efcaaa6a
MohammadHamza <qomaise3@gmail.com>
2023-10-15 20:50:58 +0500

    commit

--- carModels/serializer.py
+++ carModels/serializer.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from .models import CarModels
 
 class CarModelsListSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='model_poster.username')
+    model_poster = serializers.ReadOnlyField(source='model_poster.username')
     model_created=serializers.ReadOnlyField()
     class Meta:
         model=CarModels
-        fields=['id','model_name','model_created','user','price']
+        fields=['id','model_name','model_created','model_poster','price']
 class CarModelsDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model=CarModels
-        fields=['id','model_name','price','description','model_created','user','model_image','car_company','car_trim','car_type']
+        fields=['id','model_name','price','description','model_created','model_poster','model_image','car_company','car_trim','car_type']
 
 class CarModelsCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model=CarModels
-        fields=['id','model_name','price','description','model_created','user','model_image','car_company','car_trim','car_type']       
+        fields=['id','model_name','price','description','model_created','model_poster','model_image','car_company','car_trim','car_type']       
